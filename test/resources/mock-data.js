50c6623249ee46dc4939aec38d46edcd6aeef7e0
Sharon Funke <sharon.funke@nzz.ch>
2017-01-20 17:41:08 +0100

    Add e2e test and docker hub upload

---
+++ test/resources/mock-data.js
@@ -0,0 +1,34 @@
+module.exports = {
+	updatedDate: "2017-01-20T13:00:25.978Z",
+	title: "Stimmanteile Parlament",
+	sources: [
+		{
+			text: "some source",
+			href: "https://www.nzz.ch",
+			validHref: "true"
+		}],
+	data: {
+		subtitle: "Möglichkeit zusätzliche Informationen als Untertitel zu erfassen",
+		threshold: "5",
+		parties: [
+			{
+				name: "CDU",
+				colour: "#0084c7",
+				percentage: "30",
+				previous: "35"
+			},
+			{
+				name: "SPD",
+				colour: "#c31906",
+				percentage: "20",
+				previous: "18"
+			},
+			{
+				name: "B90/Die Grünen",
+				colour: "#66a622",
+				percentage: "10",
+				previous: "7"
+			}
+		]
+	}
+}
