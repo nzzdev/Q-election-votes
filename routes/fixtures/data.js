a90781acb341da159d0befb63a4b428d53a4db80
benib <beni.buess@nzz.ch>
2019-03-11 09:48:59 +0100

    (feat) projection data: add fixture with threshold and projection

--- routes/fixtures/data.js
+++ routes/fixtures/data.js
@@ -6,7 +6,8 @@ const fixtureData = [
   require(`${fixtureDataDirectory}/results-partly-previous-color-class.json`),
   require(`${fixtureDataDirectory}/results-long-names-threshold-others-color-codes.json`),
   require(`${fixtureDataDirectory}/results-threshold-color-codes-overwrite.json`),
-  require(`${fixtureDataDirectory}/results-partly-previous-color-class-uncertainty.json`)
+  require(`${fixtureDataDirectory}/results-partly-previous-color-class-uncertainty.json`),
+  require(`${fixtureDataDirectory}/results-threshold-color-codes-uncertainty.json`)
 ];
 
 module.exports = {
